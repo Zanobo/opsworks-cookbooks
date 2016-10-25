3c4c86051b2dd210746318a803d13871f0716cf8
Nolan Zandi <nolan@zandi.com>
2016-10-24 19:02:51 -0700

    make before migration more generic

--- joyride_ecommerce/recipes/before_migration.rb
+++ joyride_ecommerce/recipes/before_migration.rb
@@ -1,14 +1,14 @@
-include_recipe 'deploy'
-include_recipe 'deploy::rails'
+Chef::Log.info("Running deploy/before_migrate.rb...")
 
-Chef::Log.info("Running deploy/before_migrate.rb in myapp app...")
-ENV['RAILS_ENV'] = node[:deploy][:myapp][:rails_env]
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("Precompiling assets for #{rails_env}...")
+
 current_release = release_path
 
 execute "rake assets:precompile" do
   cwd current_release
   command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => node[:deploy][:myapp][:rails_env]
+  environment "RAILS_ENV" => rails_env
 end
 
 
